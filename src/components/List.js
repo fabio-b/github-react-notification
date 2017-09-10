c350aa9dee51c66863218688176b04fb9f5494c2
Fabio Bertone <fbertone@coreinformatics.com>
2017-09-10 19:45:09 -0400

    adding additional information for list component to output

--- src/components/List.js
+++ src/components/List.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import moment from 'moment';
+import './List.css';
 
 export default class List extends React.Component {
     render() {
         const listItems = this.props;
-        console.log(Object.keys(listItems));
+        // console.log(listItems);
         return (
             <ul>
-                {Object.keys(listItems).map((key) => {
-                    const listItem = listItems[key];
-                    return (
-                        <li key={listItem.id}>
-                            <img src={listItem.actor.avatar_url} alt={listItem.actor.display_login} title={listItem.actor.display_login} width="50" height="50" />
-                            {listItem.type}
-                        </li>
-                    )
+                {this.props.events.map(key => {
+                    const listItem = key;
+                    if(listItems.eventType === '' || listItems.eventType === listItem.type) {
+                        return (
+                            <li className="row" key={listItem.id}>
+                                <div className="col-xs-2">
+                                    <img src={listItem.actor.avatar_url} alt={listItem.actor.display_login} title={listItem.actor.display_login} width='100%' />
+                                </div>
+                                <div className="col-xs-9">
+                                    {moment(listItem.created_at).format('YYYY-MM-DD hh:mm A')}
+                                    <br/>
+                                    {listItem.type}
+                                </div>
+                            </li>
+                        )
+                    }else {
+                        return null;
+                    }
                 })}
             </ul>
         )
